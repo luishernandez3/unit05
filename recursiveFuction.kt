39c1753c33527940660a7be1f128285751d72ccf
luishernandez3 <123015178+luishernandez3@users.noreply.github.com>
2023-03-29 23:46:56 -0500

    Create recursiveFuction.kt

---
+++ recursiveFuction.kt
@@ -0,0 +1,9 @@
+fun fibonacci(number: Int): Int {
+    if (number <= 0) {
+        return 0
+    }
+    if (number == 1 || number == 2) {
+        return 1
+    }
+    return fibonacci(number - 1) + fibonacci(number - 2)
+}
